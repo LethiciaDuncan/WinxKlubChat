028ed2a8c1af95011ac8c1e8da2972b884010a38
LethiciaDuncan <lduncan@student.neumont.edu>
2023-05-08 09:41:02 -0600

    Add project files.

---
+++ index.php
@@ -0,0 +1,37 @@
+
+<!Doctype html>
+    <html lang="en">
+    <head>
+        <link rel="stylesheet" type="text/css"  href="/Style.css">
+        <link rel="stylesheet" type="text/css"  href="https://fonts.googleapis.com/css2?family=Rowdies:wght@300&display=swap">
+        <style>
+            body{
+                font-family: 'Rowdies', cursive;
+            }
+            pre{
+                font-family: 'Rowdies', cursive;
+            }
+        </style>
+    </head>
+        
+    <body>
+        <h1 class="display-4" id="title"> Winx Klub</h1>
+        <button class="btn btn-light btn-lg" id="LogButton">Login
+        </button>
+        <button class="btn btn-light btn-lg" id="SignButton">SignUp
+        </button>
+      
+        <pre id="bodyText">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ullamcorper tempor arcu quis mollis. Aliquam congue odio vitae orci volutpat ornare.
+        Integer leo justo, volutpat a lacinia quis, ultricies ac mi. Fusce condimentum, nisl at varius mattis, nibh lorem congue massa, ac vestibulum ipsum tortor ut metus.
+         Nam ultrices quam quis viverra commodo. Vestibulum et posuere ligula. Duis maximus, sapien eu porta faucibus, nibh est maximus orci, eget dignissim leo dui eu nibh.
+        Sed laoreet sapien purus, non sodales augue porttitor non. Mauris sit amet convallis felis. Vivamus mollis pharetra porta. Mauris laoreet dolor risus, pretium faucibus dui dapibus ac. 
+        Fusce quis condimentum ante.
+
+        </pre>
+
+        </body>
+
+        </html>
+
+
