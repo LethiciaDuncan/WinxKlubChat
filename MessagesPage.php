6f9aa0b9e7babcb5ef5ed0420b444278f55bfb60
pullejam <79878433+pullejam@users.noreply.github.com>
2023-05-17 23:06:37 -0600

    MessageCode

--- MessagesPage.php
+++ MessagesPage.php
@@ -28,8 +28,65 @@ include_once('Header.php');
             </div>
             <input type="submit" value="Submit" />
          </form>
+    <?php
+            if(array_key_exists("ChatId", $_GET)){
+            $chatId = $_GET['ChatId'];
+            echo "<div id='ChatId' hidden>" . $chatId . "</div>";
+        }
+    ?>
+
+    <script>
+    var request = new XMLHttpRequest();
+
+    window.onload = (event) => {
+        loadJson()
+    }
+
+    function loadJson() {
+        let chatId = document.getElementById("ChatId").innerText;
+        if (chatId) {
+             request.open('GET', './Backend/GetMessagesInChat.php?ChatId=' + chatId);
+                    request.onload=loadComplete;
+                    request.send();
+
+        }
+       
+
+    }
+
+
+    function loadComplete(evt) {
+        var myResponse;
+        var myData;
+        var myReturn = "";
+
+        myResponse = request.responseText;
+        console.log(myResponse)
+        //alert("A: " + myResponse); // Use for debugging
+        //document.getElementById("A").innerHTML = myResponse; // Display the json for debugging
+        myData = JSON.parse(myResponse);
+        console.log(myData)
+        // alert(myData);
+
+        // Loop through each json record and create the HTML
+
+        //for (index in myData) {
+         //   console.log(myData)
+        //    myReturn += "<tr><td>" +
+       //         myData[index].Id + "</td><td>" +
+        //        myData[index].name + "</td><td>" +
+       //         myData[index].phoneNumber + "</td></tr>";
+
+       // }
+       // myReturn += "</table>";
+        //document.getElementById("result").innerHTML = myReturn;// Display table
+
+    }
+    </script>
     
         </body>
+
+       
 
 <?php
 include_once('Footer.php');
