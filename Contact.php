28d00abdb2f59990aec0c59b4b602a9ccfe2889e
ChristopherMacias <80134430+ChristopherMacias@users.noreply.github.com>
2023-05-17 23:08:04 -0600

    updated UI

--- Contact.php
+++ Contact.php
@@ -17,8 +17,12 @@
         
          <div class =" headerDiv"></div>
         <img id ="img" src="/Images/logo.png" style="width:50px;height:35px;">
-        <h1 class="display-4" id="title"> Winx Klub</h1>
-        <button class="btn btn-light btn-lg" onclick="redirectContact()" id="ContactButton">Contact
+        <h1 class="display-4" id="title"> Winx Klub Contact Page</h1>
+        <h3 class="d1" id="James">James Pulley. Phone Number: 202-555-0370, Email: jpulley@student.neumont.edu </h3>
+        <h3 class="d2" id="Lethicia">Lethicia Duncan. Phone Number: 202-555-0600, Email: lduncan@student.neumont.edu </h3>
+        <h3 class="d3" id="Moises">Moises Olea. Phone Number: 202-555-0056, Email: molea@student.neumont.edu</h3>
+        <h3 class="d4" id="Chris">Christopher Macias. Phone Number: 202-555-0397, Email: cmacias@student.neumont.edu</h3>
+        <button class="btn btn-light btn-lg" onclick="redirectIndex()" id="HomeButton">Home
         </button>
         <button class="btn btn-light btn-lg" onclick="redirectAbout()" id="AboutButton">About
         </button>
@@ -28,7 +32,7 @@
         </button>
       
         <pre id="bodyText">
-        Welcome to Winx Klub Chat. This is a site where you can message other people. 
+        Welcome to Winx Klub Contact page. This is a page where you can find information of the creators of the sight. 
         Please log in if you already have an account, else please use the sign up button to get started.
 
         </pre>
@@ -39,12 +43,10 @@
             </footer>
         </div>
         <script>function redirectLogin() { window.location.href = "Login.php"; }
-            function redirectSignin() { window.location.href = "Signup.php"; } 
-             function redirectAbout() {window.location.href = "About.php"; }
-            function redirectContact() { window.location.href = "Contact.php"; }</script>
+            function redirectSignin() { window.location.href = "Signup.php"; }
+            function redirectAbout() {window.location.href = "About.php"; }
+            function redirectIndex() {window.location.href = "index.php"; } </script>
         
         </body>
 
         </html>
-
-
--- Login.php
+++ Login.php
@@ -8,11 +8,27 @@ if(array_key_exists("alreadyTriedU", $_GET)){
     echo "<div class='alert alert-danger'>User could not be found.</div>";
 }
 ?>
-<form action="Backend/login.php" method="post">
-    <input placeholder="Enter your username" name="username" id="username" />
-    <input placeholder="Enter your username" name="password" id="password" />
-    <input type="submit" placeholder="Login" />
+<head>
+    <link rel="stylesheet" type="text/css" href="/Style.css" />
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css2?family=Rowdies:wght@300&display=swap" />
+    <style>
+            body{
+                font-family: 'Rowdies', cursive;
+            }
+            pre{
+                font-family: 'Rowdies', cursive;
+            }
+    </style>
+</head>
+<body>
+   
+    <img id="img" src="/Images/logo.png" style="width:50px;height:35px;" />
+    <h1 class="display-4" id="title"> Log In</h1>
+<form class="" action="Backend/login.php" method="post">
+    <input  placeholder="Enter your username" class="u1" name="username" id="username" />
+    <input placeholder="Enter your password"  class="p1" name="password" id="password" />
+    <input type="submit" class="btnlog" placeholder="Login" />
 </form>
-
+</body>
 
 
