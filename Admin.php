7dd14e1456928f3c83d55f5f99fea363d35037a7
pullejam <79878433+pullejam@users.noreply.github.com>
2023-05-18 20:06:54 -0600

    AdminUpdates

---
+++ Admin.php
@@ -0,0 +1,81 @@
+
+<?php
+include_once('Header.php');
+//Add Delete Edit chats
+//Add Delete Edit Messages
+?>
+<!Doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="/Style.css" />
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css2?family=Rowdies:wght@300&display=swap" />
+    <style>
+            body{
+                font-family: 'Rowdies', cursive;
+            }
+            pre{
+                font-family: 'Rowdies', cursive;
+            }
+    </style>
+</head>
+
+<body>
+    <h1 class="display-4" id="title"> Who you are doesn't matter, but what you do does, LETS SEE WHAT U WILL MANAGE TODAY!'</h1>
+    
+
+</body>
+</html>
+<?php
+include_once('Footer.php');
+?>
+
+<script>
+    var request = new XMLHttpRequest();
+
+    window.onload = (event) => {
+        loadJson()
+    }
+
+
+
+
+    function deleteChat() {
+
+    }
+
+    function deleteMessage() {
+
+    }
+
+    function EditChat() {
+
+    }
+
+    function EditMessage() {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    function loadJson() {
+
+    }
+
+
+    function loadComplete(evt) {
+       
+    }
+</script>
+
+
+
